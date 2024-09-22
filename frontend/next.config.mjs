e73ba53c5eb527c1f10b6389c98e13684bc97d47
Grandiv <grandivfarand@gmail.com>
2024-09-22 21:40:14 +0700

    feat: image domains

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   images: {
     domains: [
+      // Kalau nambah gambar, pastiin domain ada di sini
       "lh3.googleusercontent.com",
       "s3.us-west-2.amazonaws.com",
       "www.google.com",
