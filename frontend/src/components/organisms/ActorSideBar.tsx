dd152f8070e82a574cb0af57e297b3a35d85efa3
Muhammad Farrel Akbar <142593668+MFarrelAkbar1@users.noreply.github.com>
2024-09-23 08:09:09 +0700

    added DirectorList, ActorSidebar, page for directors and profile so far (unfinished)

--- frontend/src/components/organisms/ActorSideBar.tsx
+++ frontend/src/components/organisms/ActorSideBar.tsx
@@ -2,7 +2,7 @@ import { AiOutlineFileAdd } from "react-icons/ai";
 import { AiOutlineLogout } from "react-icons/ai";
 export default function ActorSideBar(){
     return(
-        <div className="p-[24px] h-screen justify-between flex flex-col w-fit border-r-[1px]">
+        <div className="px-[24px] h-screen justify-between flex flex-col w-fit border-r-[1px] pt-[100px] pb-[24px]">
             <AiOutlineFileAdd className="text-[32px]"/>
             <AiOutlineLogout className="text-[32px]"/>
         </div>
