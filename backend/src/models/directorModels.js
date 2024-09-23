b00146e93f98d8f329de2066b64dbb5b1946a02e
ranisedangbekerja <raninirmalaprakoso@mail.ugm.ac.id>
2024-09-23 16:21:18 +0700

    rani izin masukin director

---
+++ backend/src/models/directorModels.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const directorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+  },
+  birth: {
+    date: {
+      type: Date,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+  },
+  images: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  movies: [
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Movie',
+    }
+  ]
+});
+
+module.exports = mongoose.model("Director", directorSchema);
